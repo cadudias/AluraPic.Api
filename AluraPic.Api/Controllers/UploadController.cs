30b4c864aa2888cfd5fe22e45c0c2099c16a7c34
Ricardo Hofstetter Dias <ricardo.dias@lambda3.com.br>
2020-07-13 10:46:11 -0300

    adicionada base de upload de arquivos

---
+++ AluraPic.Api/Controllers/UploadController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AluraPic.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : Controller
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult Upload(Photo photo)
+        {
+            return Ok();
+        }
+    }
+}
--- AluraPic.Api/Startup.cs
+++ AluraPic.Api/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +8,12 @@ using AluraPic.Api.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
@@ -30,6 +34,12 @@ namespace AluraPic.Api
         {
             services.AddCors(opt => opt.AddDefaultPolicy(builder =>
                builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
+
+            services.Configure<FormOptions>(o => {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
 
             //var key = Encoding.ASCII.GetBytes(Settings.Secret);
             //services.AddAuthentication(x =>
@@ -64,6 +74,13 @@ namespace AluraPic.Api
             app.UseRouting();
 
             app.UseCors();
+
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+                RequestPath = new PathString("/Resources")
+            });
 
             app.UseAuthorization();
             app.UseAuthentication();
